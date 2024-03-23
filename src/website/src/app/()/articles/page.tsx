da913dc0b08b142c678824192102820cf6dd6e78
LoucasMaillet <loucas.maillet.pro@gmail.com>
2024-03-23 18:05:23 +0100

    Return back to submodule

--- src/website/src/app/()/articles/page.tsx
+++
@@ -1,7 +0,0 @@
-import { articleInfos, articleTags } from '@/lib/articles'
-
-import Client from './client'
-
-export default function ArticleSearchPage() {
-	return <Client articleInfos={articleInfos} articleTags={articleTags} />
-}
--- src/website/src/app/(home)/client.tsx
+++
@@ -1,5 +0,0 @@
-'use client'
-
-import MaskHandler from '@/components/MaskHandler'
-
-export const { MaskedDiv, MaskDiv } = new MaskHandler()
