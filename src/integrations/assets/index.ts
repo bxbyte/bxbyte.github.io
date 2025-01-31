a5618ee7a3c13a75430738626ac74dfcc60c6a5e
bxbyte <lucas.maillet.app@gmail.com>
2025-01-31 08:46:25 +0100

    Doc

--- src/integrations/assets/index.ts
+++ src/integrations/assets/index.ts
@@ -17,21 +17,30 @@ export const assetStore: AssetStore = ((global as any)['assetStore'] ||= {})
 assetStore.cachedIdsByKeys ||= {}
 assetStore.assets ||= {}
 
-export async function setDynamicPaths<T>(
-	computePaths: () => Promise<
+/**
+ * Generate dynamic assets to share to the client.
+ *
+ * @param compute Compute function, called when the storeKey isn't set
+ * @param extension Asset's file extension
+ * @param storekey A store key, must be unique to cache results from the compute function, if undfined, the assets will always be regenerated
+ * @returns Return the computed values + the corresponding href
+ */
+export async function computeAssets<T>(
+	compute: () => Promise<
 		({
+			// Value to share as assets
 			value: any
 		} & T)[]
 	>,
 	extension: string,
-	cacheKey: any,
+	storekey: any,
 ): Promise<(T & { href: string })[]> {
-	const hashKey = hash(cacheKey)
+	const hashKey = hash(storekey)
 	if (hashKey && hashKey in assetStore.cachedIdsByKeys) {
 		return assetStore.cachedIdsByKeys[hashKey]
 	} else
 		return (assetStore.cachedIdsByKeys[hashKey] = await Promise.all(
-			(await computePaths()).map(async (props) => {
+			(await compute()).map(async (props) => {
 				const id =
 						createHash('sha256')
 							.update(props.value)
@@ -42,7 +51,7 @@ export async function setDynamicPaths<T>(
 						extension,
 					href = assetStore.assetPrefix + id
 
-				// If we're in dev mode, just push assets into the virtual route
+				// If we're in dev mode, just cache the assets into the virtual route
 				if (import.meta.env.DEV) assetStore.assets[id] = props.value
 				// Otherwise write real image's files
 				else await writeFile(join(process.cwd(), 'dist', href), props.value)
