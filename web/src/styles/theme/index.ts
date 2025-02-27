776c16241649955515a381a72b419d055dafcbc5
bxbyte <lucas.maillet.app@gmail.com>
2025-02-27 14:28:33 +0100

    Remove unused files

--- web/src/styles/theme/index.ts
+++ web/src/styles/theme/index.ts
@@ -3,4 +3,3 @@ import grainUrl_ from "./assets/grain.svg?url"
 export const grainUrl = `url(${grainUrl_})`
 
 export * from "./assets/Gradient.global.astro"
-export { default } from "./theme.module.scss"
